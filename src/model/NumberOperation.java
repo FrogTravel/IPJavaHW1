524b4f9bdfe082241b351cccfdb607c57a0164d5
FrogTravel <e.levchenko@innopolis.ru>
2018-09-26 22:38:55 +0900

    Исправлены всякие мелкости, добавленны комменты

--- src/model/NumberOperation.java
+++ src/model/NumberOperation.java
@@ -3,13 +3,26 @@ package model;
 public class NumberOperation implements Operation {
     private float sum;
 
+    /**
+     * Обработчик с числами
+     * @param s новый токен под обработку
+     * @return всегда true так как нет случая ошибок
+     */
     @Override
     public boolean process(String s) {
-        float number = Float.valueOf(s);
+       countSum(Float.valueOf(s));
+       return true;
+    }
+
+    /**
+     * Считаем сумму всех чисел. Так как результат нам не нужен то возвращает void. Но легко если нужно вытащить sum.
+     * Суммирует все четные числа.
+     * @param number с чем суммировать
+     */
+    private void countSum(Float number){
         if(number % 2 == 0){//Если число четное
             sum += number;
-            System.out.println(sum);
+            System.out.println(sum);//Печатаем каждое число
         }
-        return true;
     }
 }
--- src/model/TextOperation.java
+++ src/model/TextOperation.java
@@ -3,14 +3,32 @@ package model;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Класс обработки текстовых операций.
+ */
 public class TextOperation implements Operation {
     private Set<String> uniqueWords = new HashSet<>();
+
+    /**
+     * Обработчик операций с текстом
+     * @param s новый токен под обработку
+     * @return true - если не нужно завершать, false если нужно завершить и слова не уникальны
+     */
     @Override
     public boolean process(String s) {
-        boolean isSuccesfullAdd = uniqueWords.add(s);
+        boolean isSuccesfullAdd = checkUniqueness(s);
         if(!isSuccesfullAdd)
             System.out.println("Слова не уникальны! Повторение в слове " + s);
         return isSuccesfullAdd;
+    }
+
+    /**
+     * Для разделения функциональности, все такое. Проверяется все через HashSet.
+     * @param s то что нужно проверить на уникальность
+     * @return true - если уникально
+     */
+    private boolean checkUniqueness(String s){
+        return uniqueWords.add(s);
     }
 
 }
